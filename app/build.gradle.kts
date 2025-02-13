a86361e7bedfd6906fcd6a024665f553da761d96
litheetha <litheekadevelop@gmail.com>
2025-02-13 08:46:22 +0530

    image upload and get it's url

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.google.material)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
