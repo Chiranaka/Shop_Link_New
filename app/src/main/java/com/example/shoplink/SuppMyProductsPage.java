68af07dfee1bb70b49457ee4cbb1a30b197842ab
Rashin2002 <rashinprashastha2002@gmail.com>
2025-02-14 22:38:41 +0530

    shop interfaces

--- app/src/main/java/com/example/shoplink/SuppMyProductsPage.java
+++ app/src/main/java/com/example/shoplink/SuppMyProductsPage.java
@@ -64,9 +64,11 @@ public class SuppMyProductsPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_supp_my_products_page);
-
-
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         SharedPreferences sharedPreferences = getSharedPreferences("Prefs", MODE_PRIVATE);
         String sn = sharedPreferences.getString("supplierName", "Not Found");
